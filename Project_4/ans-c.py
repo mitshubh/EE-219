3503748a1cb7316f506a2325b251c3da46c7456a
amehrotra91 <anshita.mehrotra@gmail.com>
2017-03-05 20:12:41 -0800

    Add files via upload
    1) Updated results for SVM reduction 
    2) removed transpose during calculation of variance

--- Project_4/ans-c.py
+++ Project_4/ans-c.py
@@ -14,6 +14,7 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk import word_tokenize
 import numpy as np
 import string
+import scipy
 
 def stemList(stringList):
     stemmer = SnowballStemmer("english")
@@ -40,10 +41,23 @@ total_tfidf = tfidf_transformer.fit_transform(total_counts)
 
 total_labels = total.target//4; # Integer Division
 
+print("printing dimensions for tfidf")
+print(total_tfidf.shape)
+
 # Checking if some permutation on rows makes the matrix almost diagonal
 # perm = np.arange(total_counts.shape[0])
 # permutated_tfidf = total_tfidf[perm,:]
 # permutated_labels = total_labels[perm]
+
+print("Scipy")
+U, s, V = scipy.sparse.linalg.svds(total_tfidf)
+print(U.shape)
+print(s.shape)
+print(V.shape)
+print(s)
+dimension, = s.shape
+
+
 
 print("Performing Analysis for SVD\n")
 # Singular Value Decomposition -----------------------
@@ -51,44 +65,58 @@ print("Performing Analysis for SVD\n")
 compArr = [2,10,50,100,500]
 for comp in compArr:
     svd_model = TruncatedSVD(n_components=comp)
-    lsi = svd_model.fit(total_tfidf.T)
+    lsi = svd_model.fit(total_tfidf)
     explained_variance = svd_model.explained_variance_ratio_.sum()
     print("\nExplained variance of the SVD step with {} components: {}%".format(comp, int(explained_variance * 100)))
 
-# Since we're interested in capturing at least 25-35% variance, we pick number of components as 100
+#Since we're interested in capturing at least 25-35% variance, we pick number of components as 100
 dims = 100;
+result = 2;
+hom_result = 0;
+conf = [] ;
 for comp in np.arange(2,dims):
     svd_model = TruncatedSVD(n_components=comp, random_state=42)    # SVD model, k=comp
     lsi = svd_model.fit_transform(total_tfidf)  #Apply LSI
     kmeans = KMeans(n_clusters=2).fit(lsi)
-    conf_mat = confusion_matrix(total_labels, kmeans.labels_);  #Confusion Matrix
-    print("For dimension: ", comp)    
-    print("Confusion Matrix -- \n", conf_mat)
-    #Results
-    print("homogeneity score -- ", cluster.homogeneity_score(total_labels, kmeans.labels_)) # homogeneity score
-    print("completeness score -- ", cluster.completeness_score(total_labels, kmeans.labels_)) # completeness score
-    print("adjusted rand score -- ", cluster.adjusted_rand_score(total_labels, kmeans.labels_)) # adjusted rand score 
-    print("adusted mutual info score -- ", cluster.adjusted_mutual_info_score(total_labels, kmeans.labels_)) # adusted mutual info score
+    conf_mat = confusion_matrix(total_labels, kmeans.labels_)  #Confusion Matrix
+    score = cluster.homogeneity_score(total_labels, kmeans.labels_)
+    #print("For dimension: ", comp)    
+    #print("Confusion Matrix -- \n", conf_mat)
     
-
-# Analyzing NMF ------------------------------------
-compArr = [2,10,50,100,500]
-for comp in compArr:
-    nmf_model = NMF(n_components=comp, init='random', random_state=0)    # NMF model, k=comp
-    lsi = nmf_model.fit(total_tfidf.T)
-    print("\nReconstrction error with {} components: {}%".format(comp, nmf_model.reconstruction_err_))
-
-
-dims = 100;
-for comp in np.arange(2,dims):
-    nmf_model = NMF(n_components=2, init='random', random_state=0)    # NMF model, k=comp
-    lsi = svd_model.fit_transform(total_tfidf)  #Apply LSI
-    kmeans = KMeans(n_clusters=2).fit(lsi)
-    conf_mat = confusion_matrix(total_labels, kmeans.labels_);  #Confusion Matrix
-    print("For dimension: ", comp)    
-    print("Confusion Matrix -- \n", conf_mat)
+    if(score > hom_result):
+        hom_result = score
+        result = comp
+        conf = conf_mat
+    
     #Results
-    print("homogeneity score -- ", cluster.homogeneity_score(total_labels, kmeans.labels_)) # homogeneity score
-    print("completeness score -- ", cluster.completeness_score(total_labels, kmeans.labels_)) # completeness score
-    print("adjusted rand score -- ", cluster.adjusted_rand_score(total_labels, kmeans.labels_)) # adjusted rand score 
-    print("adusted mutual info score -- ", cluster.adjusted_mutual_info_score(total_labels, kmeans.labels_)) # adusted mutual info score
+    #print("homogeneity score -- ", cluster.homogeneity_score(total_labels, kmeans.labels_)) # homogeneity score
+    #print("completeness score -- ", cluster.completeness_score(total_labels, kmeans.labels_)) # completeness score
+    #print("adjusted rand score -- ", cluster.adjusted_rand_score(total_labels, kmeans.labels_)) # adjusted rand score 
+    #print("adusted mutual info score -- ", cluster.adjusted_mutual_info_score(total_labels, kmeans.labels_)) # adusted mutual info score
+#hom_result = 0.80222654947895511
+#resut = 65
+#conf = 3.791000000000000000e+03	1.120000000000000000e+02
+#1.300000000000000000e+02	3.849000000000000000e+03
+
+    
+## Analyzing NMF ------------------------------------
+#compArr = [2,10,50,100,500]
+#for comp in compArr:
+#    nmf_model = NMF(n_components=comp, init='random', random_state=0)    # NMF model, k=comp
+#    lsi = nmf_model.fit(total_tfidf.T)
+#    print("\nReconstrction error with {} components: {}%".format(comp, nmf_model.reconstruction_err_))
+
+
+#dims = 100;
+#for comp in np.arange(2,dims):
+#    nmf_model = NMF(n_components=2, init='random', random_state=0)    # NMF model, k=comp
+#    lsi = svd_model.fit_transform(total_tfidf)  #Apply LSI
+#    kmeans = KMeans(n_clusters=2).fit(lsi)
+#    conf_mat = confusion_matrix(total_labels, kmeans.labels_);  #Confusion Matrix
+#    print("For dimension: ", comp)    
+#    print("Confusion Matrix -- \n", conf_mat)
+#    #Results
+#    print("homogeneity score -- ", cluster.homogeneity_score(total_labels, kmeans.labels_)) # homogeneity score
+#    print("completeness score -- ", cluster.completeness_score(total_labels, kmeans.labels_)) # completeness score
+#    print("adjusted rand score -- ", cluster.adjusted_rand_score(total_labels, kmeans.labels_)) # adjusted rand score 
+#    print("adusted mutual info score -- ", cluster.adjusted_mutual_info_score(total_labels, kmeans.labels_)) # adusted mutual info score
